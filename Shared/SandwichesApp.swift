eb12a3c29f202bc02b6471831a45c6ebaed3ea94
Ben Freiband <bfreiband@duosecurity.com>
2020-06-24 14:14:58 -0400

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Ben Freiband on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
